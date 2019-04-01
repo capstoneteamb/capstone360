822e53b34f297d70b42a6e336209f6ba6910f1f6
Jacob Crisan <jacob.crisan@gmail.com>
2019-04-01 15:57:22 -0700

    Changed the setup_mockup_db and test_setup_mockup_db files again

--- setup_mockup_db.py
+++ setup_mockup_db.py
@@ -76,8 +76,7 @@ example_review = {
 }
 
 def generate_student_data():
-    id_num = 1
-    team_num = 1
+    id_num = 0
     student_data = []
     for name in names:
         # Generate email address
@@ -88,19 +87,8 @@ def generate_student_data():
         student_id = id_num
         id_num = id_num + 1
 
-        # Get team number
-        #team_number = team_num
-        #if (team_num == 6):
-        #    team_num = 1
-        #else:
-        #    team_num = team_num + 1
-
         # Add Data To List
-        student_data.append({"name": name, "email_address": email_address, "id": student_id})#, team_number))
-
-        # Debug
-        #print((name, email_address, student_id, team_number))
-        #print(teams["Team " + str(team_number)])
+        student_data.append({"name": name, "email_address": email_address, "id": student_id})
 
     return student_data
 
@@ -179,23 +167,23 @@ def submit_review(cursor, student_id, session_id, review):
                     'VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'),
                        (datetime.datetime.now(),
                         session_id,
-                        review["reporting"], 
-                        review["tid"], 
-                        review["report_for"], 
-                        review["tech_mastery"], 
-                        review["work_ethic"], 
-                        review["communication"], 
-                        review["cooperation"], 
-                        review["initiative"], 
-                        review["team_focus"], 
-                        review["contribution"], 
-                        review["leadership"], 
-                        review["organization"], 
-                        review["delegation"], 
-                        review["points"], 
-                        review["strengths"], 
-                        review["weaknesses"], 
-                        review["traits_to_work_on"], 
+                        review["reporting"],
+                        review["tid"],
+                        review["report_for"],
+                        review["tech_mastery"],
+                        review["work_ethic"],
+                        review["communication"],
+                        review["cooperation"],
+                        review["initiative"],
+                        review["team_focus"],
+                        review["contribution"],
+                        review["leadership"],
+                        review["organization"],
+                        review["delegation"],
+                        review["points"],
+                        review["strengths"],
+                        review["weaknesses"],
+                        review["traits_to_work_on"],
                         review["what_you_learned"], 
                         review["proud_of_accomplishment"], 
                         review["is_final"]))
@@ -208,17 +196,18 @@ def fill_tables_with_data(cursor, student_data, num_sessions, num_teams):
 
         # Put new team entry into the database
         for team_id in range(num_teams):
-            cursor.execute('INSERT INTO teams VALUES(?,?,?)', (team_id + (session_id * num_teams), session_id, "Team " + str(team_id + (session_id * num_teams) + 1)))
+            team_number = team_id + (session_id * num_teams)
+            cursor.execute('INSERT INTO teams VALUES(?,?,?)', (team_number, session_id, "Team " + str(team_number)))
 
         # Put data into the students, teams, team members, and reports tables
         for student in student_data:
             student_id = student["id"] + (len(student_data) * session_id)
             team_id = student["id"] % num_teams + (num_teams * session_id)
-            
+
             # Add student to the students table
             cursor.execute('INSERT INTO students VALUES(?,?,?,?,?,?)',
                            (student_id, team_id, session_id, student["name"], False, False))
-    
+
             # Add student to the team members table
             cursor.execute('INSERT INTO team_members VALUES(?,?,?)',
                            (team_id, student_id, session_id))
@@ -236,30 +225,64 @@ def fill_tables_with_data(cursor, student_data, num_sessions, num_teams):
 
             # Generate a report for each member of the student's team and put it
             # into the database
-            for team_member_id in range(num_teams):
-                # Get the id of the student the reports are for
-                target_student = session_id * len(student_data) + (team_member_id * num_teams)
-                
+            for team_index in range(num_teams):
+                # This is complicated. Basically, I want to use student ids
+                # and indexes and such to figure out the ids of the other
+                # students that are/will be on the current student's team.
+                #
+                # target_student_id = start + team_offset_id + offset
+                # start = the id of the first student on the team the student
+                # is on. We calulate this with the following:
+                #  -start = session_id * num_students
+                #   session * num_students helps us figure out where we start
+                #   If we have two sessions and have 40 students, and we want
+                #   to create reports for students in the second session, the
+                #   student id range we will be interested in will be 40-79.
+                #   session_id * num_students helps us get to the beginning
+                #   of the student_id range (40, in our example).
+                #  -team_id_offset = student_id_num % number_of_teams
+                #   This helps us get to the id of the first student
+                #   on the same team as the current student. If they are on the
+                #   second team, for example, their team_index will be 1, and
+                #   start = 40, start + team_offset_id = 40 + 1 = 41, which is the index of the first
+                #   student on the second team (in the current session).
+                #   team_id_offset is calculated by taking the student_id (before
+                #   it is adjusted to take multiple sessions into account) and
+                #   modulating it by the number of teams
+                #   (so team_offset_id = student_id % num_teams)
+                #  -offset = team_index * num_teams
+                #   This helps us transition to the id of the next student on
+                #   the team. If start is 41, there are four teams and
+                #   team_index = 0 target_student = 41, so we are creating a
+                #   report for the first student on the team. If
+                #   team_index = 1, target_student = 41 + (1 * 4) = 45. 45
+                #   is the index of the second member of student 41's team,
+                #   so we create a report for the second member of student
+                #   41's team.
+                start = session_id * len(student_data)
+                team_offset_id = student["id"] % num_teams
+                offset = team_index * num_teams
+                target_student_id = start + team_offset_id + offset
+
+                # Break the loop if we start going too far
+                if (target_student_id >= start + len(student_data)):
+                    break
+
                 # Create midterm report
                 new_midterm = copy.deepcopy(midterm)
-                new_midterm["report_for"] = target_student
+                new_midterm["report_for"] = target_student_id
 
                 # Create final
                 new_final = copy.deepcopy(final)
-                new_final["report_for"] = target_student
-                if (team_member_id == student_id):
+                new_final["report_for"] = target_student_id
+                if (target_student_id == student_id):
                     new_final["what you learned"] = "How to make a web app. Yah!"
-
-                # Debug
-                #print (new_midterm)
-                #print (new_final)
-                #print()
 
                 # Put midterm review into database
                 submit_review(cursor, student_id, session_id, new_midterm)
                 cursor.execute(('UPDATE students SET midterm_done = ? WHERE id = ? '
                                  'AND session_id = ?'), (True, student_id, session_id))
-    
+
                 # Put final review into database
                 submit_review(cursor, student_id, session_id, new_final)
                 cursor.execute(('UPDATE students SET final_done = ? WHERE id = ? '
