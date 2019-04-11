b7bfaa8596f737cce6cfae6c68aa3516fdf15abc
An Nguyen <anguye2@pdx.edu>
2019-04-11 14:52:17 -0700

    fixed port number

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ app.add_url_rule('/removeTeam/',
                 methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8000', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- gbmodel/queryrunner.sql
+++ gbmodel/queryrunner.sql
@@ -5,5 +5,5 @@ select * from capstone_session;
 select * from teams;
 
 select * from students;
-delete from removed_students;
+
 select * from removed_students;
