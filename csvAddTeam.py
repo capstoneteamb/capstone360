2ba339c2f868e259078eb28fdb4d8f52409eba6d
Christian <cclev2@gmail.com>
2019-04-25 08:59:31 -0700

    Made changes necessary to match coding standards

--- csvAddTeam.py
+++ csvAddTeam.py
@@ -12,31 +12,28 @@ class csvAddTeam(MethodView):
     return render_template('csvAddTeam.html')
   def post(self):
     session = gbmodel.capstone_session()
-    teamsTable = gbmodel.teams() # Accessor to the teams table
-    studentsTable = gbmodel.students() # Accessor to the students table
+    teams_table = gbmodel.teams() # Accessor to the teams table
+    students_table = gbmodel.students() # Accessor to the students table
     file = request.files['teamcsv']
     stream = io.StringIO(file.stream.read().decode("UTF8"), newline=None)
     csv_reader = csv.reader(stream, delimiter=',')
     for row in csv_reader:
-      teamName = row[0]
+      team_name = row[0]
       students = []
       i = 1 # Incrementor for while loop below
       while i < len(row):
         students.extend([row[i]])
         i+=1
       # Get date to determine session ID
-      currentDate = datetime.datetime.now()
-      month = int(currentDate.month) 
-      year = currentDate.year
+      current_date = datetime.datetime.now()
+      month = int(current_date.month) 
+      year = current_date.year
       if month in range (9, 11):   term = "Fall"
       elif month in range (3,5):   term = "Spring"
       elif month in range (6,8):   term = "Summer"
       else:                        term = "Winter"
       sessionID = session.getSessionID(term, year)
-      ''' 
-      nextID = teamsTable.getMaxTeamID() + 1
-      teamsTable.insertTeam(nextID, teamName) # Inser the new team to the DB
-      '''
+      teams_table.insertTeam(sessionID, team_name) # Insert the new team to the DB
       for s in students:
-        studentsTable.updateTeam(s, sessionID)
+        students_table.updateTeam(s, sessionID)
       return render_template('csvAddTeam.html')
