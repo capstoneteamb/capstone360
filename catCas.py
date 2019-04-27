6e981f53364e96320ac3359810e6056079e90520
Long Nguyen <Long.Nguyen@LongNs-MacBook-Pro.local>
2019-04-27 13:16:08 -0700

    removed unused imports

--- catCas.py
+++ catCas.py
@@ -1,10 +1,5 @@
-from flask import redirect, request, url_for, render_template
-from flask.views import MethodView
 import gbmodel
-import datetime
-import dashboard
 from app import cas
-from flask_cas import login_required
 
 def validate():
     username = cas.username
