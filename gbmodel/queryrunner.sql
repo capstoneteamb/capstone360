0a12ec34c8f173e85c40a8a59e458d05037b6314
An Nguyen <anguye2@pdx.edu>
2019-05-02 16:43:26 -0700

    validate dates

--- gbmodel/queryrunner.sql
+++ gbmodel/queryrunner.sql
@@ -1,5 +1,6 @@
--- SQLite
-
+-- -- SQLite
+-- INSERT INTO students (id, tid, session_id, name, is_lead, midterm_done, final_done, active)
+-- VALUES (382,1,0,'anguye2',0,0,0,'midterm');
 select * from capstone_session;
 
 select * from teams;
