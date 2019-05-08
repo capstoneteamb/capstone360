025e0904b0e8c2ba8efad13d23ace5cab04c9cd5
Christian <cclev2@gmail.com>
2019-05-07 17:01:32 -0700

    Added comments to decrypt_test

--- decrypt_test.py
+++ decrypt_test.py
@@ -7,17 +7,18 @@ key = bytes(key.encode("UTF8"))
 cipher = Fernet(key)
 
 students = gbmodel.students()
-'''
+'''  # Decerypt and print all students
 c_names = students.query.all()
 for each in c_names:
   p_name = cipher.decrypt(each.name)
   p_name = p_name.decode('UTF8')
   print(p_name)
 '''
-
+#  Decrypt and print students in session 0 team 0
 print("Team 0:")
 print(students.get_students(0, 0))
 
+#  Insert a student to the database, then decrypt and print the team with the new student
 students.insert_student("Dame Dolla", 1990, 0, "Team 0")
 print("Team 0:")
 print(students.get_students(0, 0))
