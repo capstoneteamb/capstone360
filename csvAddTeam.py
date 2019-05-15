4c836d6356706bd4d055dd0deb70ca04243eeed0
Christian <cclev2@gmail.com>
2019-05-14 18:44:45 -0700

    Fixed line that exceeded character limit.

--- csvAddTeam.py
+++ csvAddTeam.py
@@ -1,15 +1,15 @@
-from flask import redirect, render_template, request, g, url_for
+from flask import render_template, request
 from flask.views import MethodView
 import gbmodel
 import datetime
 import csv
 import io
-import os
 
 
 class AddTeamCSV(MethodView):
     def get(self):
         return render_template('csvAddTeam.html')
+
     def post(self):
         session = gbmodel.capstone_session()
         teams_table = gbmodel.teams()  # Accessor to the teams table
@@ -24,15 +24,22 @@ class AddTeamCSV(MethodView):
 
             # Get date to determine session ID
             current_date = datetime.datetime.now()
-            month = int(current_date.month) 
+            month = int(current_date.month)
             year = current_date.year
-            if month in range(9, 11):   term = "Fall"
-            elif month in range(3, 5):   term = "Spring"
-            elif month in range(6, 8):   term = "Summer"
-            else:                        term = "Winter"
+            if month in range(9, 11):
+                term = "Fall"
+            elif month in range(3, 5):
+                term = "Spring"
+            elif month in range(6, 8):
+                term = "Summer"
+            else:
+                term = "Winter"
+
             session_id = session.get_session_id(term, year)
             # Create team if it doesn't exist, then create the student.
-            if teams_table.check_dup_team(team_name, session_id) == True:
+            if teams_table.check_dup_team(team_name, session_id) is True:
                 teams_table.insert_team(session_id, team_name)
-            students_table.insert_student(student_name, "", student_id, session_id, team_name)
+            students_table.insert_student(student_name,
+                                          "", student_id,
+                                          session_id, team_name)
         return render_template('csvAddTeam.html')
