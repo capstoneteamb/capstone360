4b23d503fd0e5e5a4edd01c44943e12611e08774
Long Nguyen <Long.Nguyen@LongNs-MacBook-Pro.local>
2019-05-20 17:32:29 -0700

    removed session_id checking for logins

--- catCas.py
+++ catCas.py
@@ -1,31 +1,13 @@
 import gbmodel
-import datetime
 from flask_cas import CAS
 
 # function to grab cas username and passes the value to gbmmodel to vaidate
-# check to see if student id is in the current session
 # INPUT: none
 # OUTPUT: return False if the id does not exist
 #         return student infomation otherwise
 
 
 def validate_student():
-    session = gbmodel.capstone_session()
-
-    currentDate = datetime.datetime.now()
-    month = int(currentDate.month)
-    year = currentDate.year
-    if month in range(9, 11):
-        term = "Fall"
-    elif month in range(3, 6):
-        term = "Spring"
-    elif month in range(6, 9):
-        term = "Summer"
-    else:
-        term = "Winter"
-
-    # get current capstone session id
-    session_id = session.get_session_id(term, year)
     cas = CAS()
     username = cas.username
     students = gbmodel.students()
@@ -34,17 +16,12 @@ def validate_student():
     # if not then check if they are in the 2nd half of their capstone session
     if found_student is False:
         return False
-    elif found_student.session_id == session_id:
-        return found_student
-    elif (1 + found_student.session_id) == session_id:
-        return found_student
-    return False
+    return found_student
 
 # check to see if professor id is in the professor table
 # INPUT: none
 # OUTPUT: return False if the id does not exist
 #         return True otherwise
-
 
 def validate_professor():
     cas = CAS()
