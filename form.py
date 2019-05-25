b5d34187df32bf0833ae264ad8851c9d353187b9
Adam Barber <necrobuffalo@users.noreply.github.com>
2019-05-25 11:36:56 -0700

    Don't log PII
    Don't need this for bugfixing anymore.

--- form.py
+++ form.py
@@ -250,9 +250,6 @@ class review(MethodView):
                     dat["proud"] = report.proud_of_accomplishment
             itemize.append(dat)
 
-            print("Editing the following information: ")
-            print(itemize)
-
             return itemize
         except SQLAlchemyError:
             return None
