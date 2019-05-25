037b864056f115d4dcfcd35d50687eda66102a46
Bryan Logan <lbryan@pdx.edu>
2019-05-24 17:32:03 -0700

    Remove unnecessary import, revise review template

--- gbmodel/model_sqlalchemy.py
+++ gbmodel/model_sqlalchemy.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import datetime
-from app import db, engine, db_session  # noqa
+from app import db, engine # noqa
 from sqlalchemy import exc, func
 
 sys.path.append(os.getcwd())
