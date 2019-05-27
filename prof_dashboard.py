31a51249b93e425fc9d39dc0bcd109792a54da66
falikhan <faisal.s.alikhan@gmail.com>
2019-05-26 17:04:39 -0700

    Flake8

--- prof_dashboard.py
+++ prof_dashboard.py
@@ -152,8 +152,8 @@ class ProfDashboard(MethodView):
             while not professor.check_professor(request.form['professor_id']):
                 error = "Enter a valid professor ID"
                 return render_template('addSession.html', error=error, session_id=session_id)
-            while not session.check_dup_session(request.form['start_term'],request.form['start_year'], 
-                                                    request.form['professor_id']):
+            while not session.check_dup_session(request.form['start_term'], request.form['start_year'],
+                                                request.form['professor_id']):
                 error = "Session already exists"
                 return render_template('addSession.html', error=error, session_id=session_id)
             start_term = request.form.get('start_term')
